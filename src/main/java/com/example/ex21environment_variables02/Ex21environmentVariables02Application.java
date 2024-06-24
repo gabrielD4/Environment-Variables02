4da648e1094bb3771fb39c39b81e7275d3afa5b4
Gabriel <g.cellardoor.delloiacovo@gmail.com>
2024-06-24 16:20:33 +0200

    exercise

---
+++ src/main/java/com/example/ex21environment_variables02/Ex21environmentVariables02Application.java
@@ -0,0 +1,13 @@
+package com.example.ex21environment_variables02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex21environmentVariables02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex21environmentVariables02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ex21environment_variables02/Ex21environmentVariables02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ex21environment_variables02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex21environmentVariables02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
